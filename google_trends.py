e4fd7e6b79dbf2ea9d26323dcb6c93681742fc72
Ian Chen <ian@Ians-MacBook-Pro.local>
2019-04-17 15:32:45 +0800

    new async ver

--- google_trends.py
+++ google_trends.py
@@ -1,9 +1,9 @@
-import re
 import json
 import sys
 import datetime
+import asyncio
 
-import requests
+import aiohttp
 
 start_dt = datetime.datetime.today()
 days_int = int(sys.argv[1])
@@ -29,20 +29,34 @@ params = {
 
 data = []
 
-for x in range(days_int):
-    days = datetime.timedelta(days=x)
-    ed = (start_dt - days).strftime("%Y%m%d")
-    print(ed)
-    params['ed'] = ed
-
-    response = requests.get('https://trends.google.com/trends/api/dailytrends', headers=headers, params=params)
-    response.encoding = 'utf-8'
-    dd = response.text.split('\n')[1] # Get rid of some weird string a line above the json data..
+def parse(html):
+    dd = html.split('\n')[1] # Get rid of some weird string a line above the json data..
     sss = json.loads(dd, encoding='utf-8')
     try:
         ff = [s.get('title').get('query') for s in sss.get('default').get('trendingSearchesDays')[0].get('trendingSearches')]
-        data.extend(ff)
     except Exception as e:
         print(sss)
         sys.exit(0)
-print(data)
+    return ff
+
+async def fetch(session, url, headers, params):
+    print(f"fetching {url}")
+    async with session.get(url, headers=headers, params=params) as response:
+        return await response.text()
+
+async def main():
+    futs = []
+    async with aiohttp.ClientSession() as session:
+        for x in range(days_int):
+            days = datetime.timedelta(days=x)
+            ed = (start_dt - days).strftime("%Y%m%d")
+            print(ed)
+            params['ed'] = ed
+            futs.append(fetch(session, 'https://trends.google.com/trends/api/dailytrends', headers=headers, params=params))
+        htmls = await asyncio.gather(*futs)
+        for html in htmls:
+            data.extend(parse(html))
+    print(data)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
